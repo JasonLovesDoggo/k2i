fdbef8b9ec0da0f46d389b3d516b049ef6be0cae
Jason <git@jasoncameron.dev>
2024-07-16 18:11:26 -0400

    updated to use caddy to share

--- k2i/settings.py
+++ k2i/settings.py
@@ -175,4 +175,4 @@ logging.config.dictConfig({
 logger = logging.getLogger(__name__)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = "/static/"
