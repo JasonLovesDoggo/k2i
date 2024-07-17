e9a1af7412c2275f9e90f8466028e7bd1f87db80
Jason <git@jasoncameron.dev>
2024-07-17 16:32:33 -0400

    fixed imports

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.utils import timezone
 
 from .models import Opportunity, Resource, User
